fbb24ef33a67e2d1770b819cc34d53bd811a935f
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-10-26 15:18:18 -0500

    Header and image implemented

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import foodImage from '../../assets/food.jpg'
+import { ButtonHeader } from './ButtonHeader'
+import classes from './Header.module.css'
+export const Header = () => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>Menu</h1>
+                <ButtonHeader />
+            </header> 
+            <div className={classes['main-image']}>
+                <img src={foodImage} alt="food image" />
+            </div>
+        </>
+    )
+}
