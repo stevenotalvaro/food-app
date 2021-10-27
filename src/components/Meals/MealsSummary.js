cdcd4a4e4b2833ea1d3c526910e8cc6bcab96603
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-10-26 21:51:48 -0500

    Section center aplicated, and array with meals implemented

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './MealsSummary.module.css'
+
+export const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Food</h2>
+            <p>Laboris aliquip magna officia duis ipsum aute amet mollit reprehenderit occaecat. Tempor sit voluptate occaecat sit est nostrud ipsum nulla. Deserunt ex reprehenderit ea enim non Lorem reprehenderit nisi cillum.</p>
+            <p>Labore excepteur anim adipisicing ullamco id excepteur Lorem fugiat. Consectetur aliquip laboris sunt exercitation ad fugiat irure duis qui occaecat anim fugiat sunt. Velit eiusmod minim esse voluptate veniam aute sit eu. Exercitation deserunt cupidatat exercitation non fugiat velit nostrud ipsum in adipisicing consectetur velit do.</p>
+        </section>
+    )
+}
