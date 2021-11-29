997a3753d6e9d19e50e759e3caff07cc43bed853
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-11-28 22:19:22 -0500

    reducer aggregated

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { CartContext } from './cart-context'
 
+const initialState = {
+    items: [],
+    totalAmount: 0,
+}
+
+const cartReducer = (state = initialState, action) =>{
+
+    switch (action.type) {
+        case 'ADD':
+            return{
+                ...state,
+                items: [...action.payload],
+                totalAmount: state.totalAmount + action.payload.price * action.payload.amount
+            }
+        case 'REMOVE':
+            break
+        default:
+            return state
+    }     
+}
 
 export const CartProvider = ({children}) => {
 
-    const handlerAddItem = (item) => {}
-    const handlerRemoveItem = (id) =>{}
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, initialState)
+
+    const handlerAddItem = (item) => {
+        dispatchCartAction({type: 'ADD', payload: item})
+    }
+    const handlerRemoveItem = (id) =>{
+        dispatchCartAction({type: 'REMOVE', payload: id})
+    }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: handlerAddItem,
         removeItem: handlerRemoveItem
     }
